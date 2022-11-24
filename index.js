4c15fb92e147d1b10279e47d44dd1f88d0c954ed
duypv <duypv98@gmail.com>
2022-11-24 16:27:19 +0700

    refactor: release ghpages

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.static('public', { index: "index.html" }));
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('running');
-});
